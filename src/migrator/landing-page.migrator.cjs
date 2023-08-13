d7f7c13ca679e0f1063144e32af8b88b6a18a25f
milosz08 <gilgamilosz451@gmail.com>
2023-08-13 05:57:13 +0200

    create docker migrator for landing page

--- src/migrator/landing-page.migrator.cjs
+++ src/migrator/landing-page.migrator.cjs
@@ -24,3 +24,67 @@
  */
 
 'use strict';
+
+const utils = require('../helpers/helpers.cjs');
+const promisifyUtils = require('../helpers/promisify-utils.cjs');
+const { submodules } = require('../config.cjs');
+
+const { mode } = utils.checkInputArguments([
+  { name: 'mode', alias: 'm', type: String },
+]);
+
+utils.printBaseMigratorInfo(mode);
+
+const targetDirectory = submodules.landingPage.path;
+const containerName = submodules.landingPage.containerName;
+const outputDir = '/msph-landing-page-content/dist/';
+
+const allStages = 5;
+let currentStage = 1;
+
+async function processing() {
+  try {
+    await promisifyUtils.checkIfDockerContainerIsRunning({
+      containerName,
+      stage: currentStage++,
+      allStages,
+    });
+    await promisifyUtils.createPromisifyProcess({
+      execCommand: `yarn run docker:${mode}`,
+      messOnStart: 'Compiling vite (astro) bundles',
+      messOnEnd: 'compiled vite (astro) bundles',
+      stage: currentStage++,
+      allStages,
+      options: { cwd: targetDirectory },
+    });
+    await promisifyUtils.createPromisifyProcess({
+      execCommand: `docker exec ${containerName} rm -rf ${outputDir}`,
+      messOnStart: 'Clear docker container /dist directory',
+      messOnEnd: 'cleared docker container /dist directory',
+      stage: currentStage++,
+      allStages,
+    });
+    await promisifyUtils.createPromisifyProcess({
+      execCommand: `docker cp ${targetDirectory}/dist ${containerName}:${outputDir}`,
+      messOnStart: 'Migrate bundled content into docker container',
+      messOnEnd: 'migrated bundled content into docker container',
+      stage: currentStage++,
+      allStages,
+    });
+    await promisifyUtils.createPromisifyProcess({
+      execCommand: `docker restart ${containerName}`,
+      messOnStart: 'Restarting Astro Node web server',
+      messOnEnd: 'restarted Astro Node web server',
+      stage: currentStage++,
+      allStages,
+    });
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+processing().then(() => {
+  utils.printNewLine();
+  utils.printSuccessMessage('Migrated landing-page into docker container.');
+});
